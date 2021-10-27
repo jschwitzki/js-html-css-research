b068368edc4bc15664bc8e7a14bd035e8a9c0d46
jschwitzki <jschwitzki@hotmail.com>
2021-10-27 08:08:47 -0300

    Módulos

---
+++ Modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("ET Bilu", 22));
+console.log(mostraCidade("ET Bilu", "Corguinho"));
+console.log(mostraCidade("ET Bilu", "Conhecimento"));
